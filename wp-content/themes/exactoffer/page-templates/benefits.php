a02ae2c5cd66d64dde7c9da85e5636d4019e65b9
Joel Newcomer <joel@drumcreative.com>
2018-11-12 16:35:21 -0500

    dev

--- wp-content/themes/exactoffer/page-templates/benefits.php
+++ wp-content/themes/exactoffer/page-templates/benefits.php
@@ -4,15 +4,15 @@ Template Name: Benefits
 */
 get_header(); ?>
 
-<div id="page" role="main">
+<div class="benefits-page" id="page" role="main">
 	
 	<?php get_template_part('template-parts/benefits'); ?>
 
-	<section class="cta">
+	<section class="cta benefits-cta">
 		<div class="grid-container">
     	    <div class="grid-x grid-padding-x">
 				<div class="large-12 cell text-center">
-					<h2 class="magenta"><?php echo get_field('cta_header'); ?></h2>
+					<h2 class="white"><?php echo get_field('cta_header'); ?></h2>
 					<p><?php echo get_field('cta_blurb'); ?></p>
 				</div>
     	    </div>    	    
