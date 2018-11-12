4e405388712572ebdc2c68a52004f327fe605165
Joel Newcomer <joel@drumcreative.com>
2018-11-12 14:43:32 -0500

    dev

--- wp-content/themes/exactoffer/page-templates/how-it-works.php
+++ wp-content/themes/exactoffer/page-templates/how-it-works.php
@@ -11,22 +11,25 @@ get_header(); ?>
 	$image_id = get_post_thumbnail_id();
 	?>
 	
-	<section class="how-it-works" role="main">
+	<section class="how-it-works-page" role="main">
 		<div class="grid-container">
     	    <div class="grid-x grid-padding-x">
-				<div class="large-12 cell text-center">
-					<h2 class="teal"><?php echo get_field('header'); ?></h2>
+				<div class="large-12 cell text-center how-it-works-intro">
+					<h2 class="magenta"><?php echo get_field('header'); ?></h2>
 					<p><?php echo get_field('subheader'); ?></p>
 				</div>
     	    </div>
     	    <div class="grid-x grid-margin-x">
 				<?php if(get_field('steps')): ?>
+					<?php $i = 1; ?>
 					<?php while(has_sub_field('steps')): ?>
 						<div class="large-6 medium-6 cell text-center step-block">
+							<div class="step-num "><div><?php echo $i; ?></div></div>
 							<?php echo file_get_contents(get_sub_field('step_icon')); ?><br />
-							<h3><?php echo get_sub_field('step_title'); ?></h3
-							<p class="teal"><?php echo get_sub_field('step_blurb'); ?></p>
+							<h3><?php echo get_sub_field('step_title'); ?></h3>
+							<p><?php echo get_sub_field('step_blurb'); ?></p>
 						</div>
+						<?php $i++; ?>
 					<?php endwhile; ?>
 				<?php endif; ?>
     	    </div>														
