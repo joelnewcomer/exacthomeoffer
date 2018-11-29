5ecbac5d891eff42d246fddbdf9f114f88d5af12
Joel Newcomer <joel@drumcreative.com>
2018-11-29 09:44:48 -0500

    Footer Address

--- wp-content/themes/exactoffer/template-parts/locations.php
+++ wp-content/themes/exactoffer/template-parts/locations.php
@@ -8,6 +8,11 @@
 			<?php // function drum_smart_phone($phone, $phone_text, $phone_label)
 			echo drum_smart_phone($location['loc_phone'], $location['loc_phone'], ''); ?>
 		</h2>
+		<?php if (get_field('footer_address')) : ?>
+		<address>
+			<?php echo get_field('footer_address'); ?>
+		</address>
+		<?php endif; ?>
 		<p>
 			<?php if( $location['loc_email'] ): ?>
 				<a class="email" href="mailto:<?php echo $location['loc_email'];?>" class="email"><?php get_template_part('assets/images/email', 'icon.svg'); ?><?php echo $location['loc_email'];?></a><br />
