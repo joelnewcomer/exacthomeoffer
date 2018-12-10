a72e66f30b6b34aa3c3ef8ffb0d0fa2965ee6582
Joel Newcomer <joel@drumcreative.com>
2018-12-10 11:25:47 -0500

    tweaks

--- wp-content/themes/exactoffer/assets/javascript/footer.js
+++ wp-content/themes/exactoffer/assets/javascript/footer.js
@@ -7,7 +7,7 @@ require('./originals/back-to-top');
 // require('./originals/background-blur.min'); // Remove if you don't have and background blurs.
 require('./originals/easyResponsiveTabs');
 // require('./originals/jquery.bxslider.min'); // Remove if you don't have a slider.
-require('./originals/jquery.matchHeight-min'); 
+// require('./originals/jquery.matchHeight-min'); 
 require('./originals/jquery.slimmenu.min'); // Remove if you use a full-screen menu.
 // require('./originals/paroller'); // Remove if you don't use parallax.
 // require('./originals/preloader');
